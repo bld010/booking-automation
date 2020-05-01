92c9684164fc4f0a4fed077d8dd71935e9adce79
Brianna DelValle <brianna.delvalle@flyfrontier.com>
2020-05-01 08:35:54 -0600

    Finish booking flow, add some readme info and comments

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontier Booking Flow Automation
+
+Currently, this works for one-way bookings only. 
+
+### How To
+
+- Clone this repo
+- run `npm i` to install dependencies
+- connect to VPN
+- run IBE locally on your machine
+- in `booking-automation.js` change the `nightmare.goto()` url to your local IBE url
+- run `node booking-automation.js`
+
--- booking-automation.js
+++ booking-automation.js
@@ -26,6 +26,8 @@ var passenger1BirthDate = "1/6/1980";
 var emailAddress = "automation@flyfrontier.com";
 var phoneNumber = "555-555-5555";
 var zipCode = "80201";
+var cardNumber = "4012001021000605";
+var cvv = "123";
 
 
 
@@ -40,8 +42,11 @@ function setTripTypeSelector(tripType) {
 setTripTypeSelector(tripType);
 
 
+
 nightmare
-    .goto('https://ll-75cxnh2.flyfrontier.com/')
+    .goto('https://ll-75cxnh2.flyfrontier.com/')  //Put in your local IBE 
+
+    //#region Flight Search
     .type('#fromInput', origin)
     .wait(`[data-value="${origin}"]`)
     .click(`[data-value="${origin}"]`)
@@ -52,8 +57,16 @@ nightmare
     .wait('.hasDatepicker')
     .type('.hasDatepicker', departureDate)
     .click('#searchButton')
+    //#endregion
+
+
+    //#region Flight Select (Chooses default selection)
     .wait('#submit_flight_search_button')
     .click('#submit_flight_search_button')
+    //#endregion
+
+
+    //#region Passenger Info Page (One passenger)
     .wait('#frontierPassengers_0__Name_First')
     .click('#frontierPassengers_0__Name_First')
     .type('#frontierPassengers_0__Name_First', passenger1FirstName)
@@ -64,23 +77,87 @@ nightmare
     .type('#js_first_phone_number', phoneNumber)
     .type('#frontierContact_PostalCode', zipCode)
     .click('#submit_passenger_info_button')
+    //#endregion
+
+    //#region Bundle Page (Doesn't select a bundle)
     .wait('.js-bundlesSubmitButton')
     .click('.js-bundlesSubmitButton')
+    //#endregion
+
+    //#region Seat Selection (Doesn't select seats)
     .wait('#saveSelectedSeats')
     .click('#saveSelectedSeats')
     .wait('.f9-btn.f9-btn-link-green.cd-btn')
     .click('.f9-btn.f9-btn-link-green.cd-btn')
-    .wait('#saveSelectedSeats')
-    .click('#saveSelectedSeats')
+    //#endregion
+
+
+    //#region For one-way flights with two legs, comment in the below code
+    // .wait('#saveSelectedSeats')
+    // .click('#saveSelectedSeats')
     // .wait('.f9-btn.f9-btn-link-green.cd-btn')
     // .click('.f9-btn.f9-btn-link-green.cd-btn')
+    //#endregion
+
+
+    //#region Bag Selection (No COB, no checked bags)
     .wait('.js-bagsCarryOnSelect')
     .select('.js-bagsCarryOnSelect', 'CO_0') //CO_1 is 1 carry-on ... make dynamic
     .click('.js-bagsSubmitButton')
+    //#endregion
+
+
+    //#region Extras (No extras selected)
     .wait('.js-extrasSubmitButton')
     .click('.js-extrasSubmitButton')
-    
-    // .end()
+    //#endregion
+
+
+    //#region Payment Page
+    .wait('#cardholder_name')
+    .type('#cardholder_name', `${passenger1FirstName} ${passenger1LastName}`)
+    .type('#card_number', cardNumber)
+    .select('#card_expiration_month', "1")
+    .select('#card_expiration_year', "49")
+    .type('#card_cvv', cvv)
+
+    .type('#billing_payment_address_1', "123 Main St.")
+    .type('#billing_payment_city', 'Denver')
+    .select('#billing_payment_state', 'CO')
+
+    .click('.js-paymentSubmitButton')
+    //#endregion
+
+
+    //Close slideout (maybe?)
+    .wait('#btnReturnToBooking')
+    .click('#btnReturnToBooking')
+
+
+    //Grab PNR
+    .wait('.itin-header span.pnr')
+    .evaluate(() => {
+        pnr = document.querySelector('.itin-header span.pnr').innerText;
+        console.log("The PNR is " + pnr) // this is in the browser scope
+        return pnr;
+    })
+    .then((pnr) => {
+
+        console.log('\n\n')
+        console.table({
+            PNR: pnr,
+            LastName: passenger1LastName
+        })
+        console.log('\n\n')
+        // this is in node scope
+
+        // comment in below line if you just want a PNR (and don't want to stop at a specific part of the flow)
+        // return nightmare.end();
+    })
+
+
+
+    // This command closes the browser (Only do this if you just want a PNR)
     .then(function (result) {
         var end = new Date() - start;
         console.log('Duration: ', end);    
